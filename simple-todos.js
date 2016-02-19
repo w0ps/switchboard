0c9b23fcd2c9542547fb6cfc3e687678f965e4a8
Ivo de Kler <ivo.de.kler@gmail.com>
2016-02-19 16:26:07 +0100

    added needs

--- simple-todos.js
+++ simple-todos.js
@@ -1,5 +1,26 @@
 Tasks = new Mongo.Collection("tasks");
 
+Needs = new Mongo.Collection( 'needs' );
+
+function Post() {
+  this.createdBy = Meteor.userId();
+  this.created = new Date();
+  this.responses = [];
+}
+
+function Need( need ) {
+  this.title = need.title;
+  this.description = need.description;
+  this.createdBy = Meteor.userId();
+  // users that get notified when something happens in this thread
+  this.subscribed = [ this.createdBy ];
+  this.keywords = need.keywords || [];
+  // ids of response posts
+  this.responses = need.responses || [];
+  // other needs this need needs to be met
+  this.requirements = need.requirements || [];
+}
+
 if (Meteor.isServer) {
   // This code only runs on the server
   // Only publish tasks that are public or belong to the current user
@@ -11,11 +32,23 @@ if (Meteor.isServer) {
       ]
     });
   });
+
+  Meteor.publish( 'needs', function() {
+    return Needs.find( {} );
+  } );
 }
+
+Router.route( '/needs', function() {
+  console.log( 'rendering needs' );
+  this.render( 'needs' );
+} );
+
 
 if (Meteor.isClient) {
   // This code only runs on the client
   Meteor.subscribe("tasks");
+
+  Meteor.subscribe( 'needs' );
 
   Template.body.helpers({
     tasks: function () {
@@ -26,6 +59,9 @@ if (Meteor.isClient) {
         // Otherwise, return all of the tasks
         return Tasks.find({}, {sort: {createdAt: -1}});
       }
+    },
+    needs: function() {
+      return Needs.find( {} );
     },
     hideCompleted: function () {
       return Session.get("hideCompleted");
@@ -49,29 +85,43 @@ if (Meteor.isClient) {
       // Clear form
       event.target.text.value = "";
     },
+    "submit .new-need": function( event ) {
+      event.preventDefault();
+
+      var title = event.target.title.value,
+          description = event.target.description.value;
+
+      Meteor.call( 'addNeed', { title: title, description: description } );
+    },
     "change .hide-completed input": function (event) {
       Session.set("hideCompleted", event.target.checked);
     }
   });
 
-  Template.task.helpers({
-    isOwner: function () {
-      return this.owner === Meteor.userId();
-    }
-  });
+  // Template.task.helpers({
+  //   isOwner: function () {
+  //     return this.owner === Meteor.userId();
+  //   }
+  // });
 
-  Template.task.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Meteor.call("setChecked", this._id, ! this.checked);
-    },
-    "click .delete": function () {
-      Meteor.call("deleteTask", this._id);
-    },
-    "click .toggle-private": function () {
-      Meteor.call("setPrivate", this._id, ! this.private);
-    }
-  });
+  // Template.task.events({
+  //   "click .toggle-checked": function () {
+  //     // Set the checked property to the opposite of its current value
+  //     Meteor.call("setChecked", this._id, ! this.checked);
+  //   },
+  //   "click .delete": function () {
+  //     Meteor.call("deleteTask", this._id);
+  //   },
+  //   "click .toggle-private": function () {
+  //     Meteor.call("setPrivate", this._id, ! this.private);
+  //   }
+  // });
+  
+  Template.need.events( {
+    // 'click input[type="submit"]': function() {
+
+    // }
+  } );
 
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
@@ -91,6 +141,13 @@ Meteor.methods({
       owner: Meteor.userId(),
       username: Meteor.user().username
     });
+  },
+  addNeed: function( need ) {
+    if ( !Meteor.userId() ) throw new Meteor.Error( 'not-authorized' );
+
+    //need = new Need( need );
+
+    Needs.insert( new Need( need ) );
   },
   deleteTask: function (taskId) {
     var task = Tasks.findOne(taskId);
