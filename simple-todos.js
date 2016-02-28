782fd0b299ce07b3d2cd443f777128dacca69a60
Ivo de Kler <ivo.de.kler@gmail.com>
2016-02-28 21:35:07 +0100

    avatars everywhere

--- simple-todos.js
+++ simple-todos.js
@@ -73,8 +73,17 @@ function userIdToUserName( userId ) {
   return Meteor.users.findOne( { _id: userId} ).username;
 }
 
+function getAvatar( userId ) {
+  var user = Meteor.users.findOne( { _id: userId } );
+  return '<img src="' + user.avatar + '" alt="' + user.username + '" />';
+}
+
 function formatTime( date ) {
   return date.toTimeString().substring( 0, 8 );
+}
+
+function isOwner() {
+  return this.createdBy && this.createdBy === Meteor.userId();
 }
 
 if (Meteor.isClient) {
@@ -89,9 +98,13 @@ if (Meteor.isClient) {
 
   Template.registerHelper( 'userIdToUserName', userIdToUserName );
 
+  Template.registerHelper( 'getAvatar', getAvatar );
+
   Template.registerHelper( 'formatTime', formatTime );
 
   Template.registerHelper( 'log', console.log.bind( console ) );
+
+  Template.registerHelper( 'isOwner', isOwner );
 
   Template['need-detail'].helpers({
     // log: function( something ) {
