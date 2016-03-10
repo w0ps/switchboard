abd9ac9643c50de53ffec196f8acb7da4f96d5f6
Ivo de Kler <ivo.de.kler@gmail.com>
2016-03-10 18:29:12 +0100

    scroll follows chat messages when at bottom

--- client/chat.js
+++ client/chat.js
@@ -19,13 +19,58 @@ Template[ 'need-detail' ].events( {
     var value = event.target.value;
     if( !value ) return;
     event.target.value = '';
-    Meteor.call( 'stopsTyping' );
+    isWriting = false;
+    Meteor.call( 'stopTyping' );
     Meteor.call( 'addChatMessage', value, this.need()._id );
   }
 } );
 
-Template[ 'chat' ].helpers( {
+Template.chat.helpers( {
 	whose: function() {
 		return this.createdBy === Meteor.userId() ? ' mine' : '';
 	}
 } );
+
+scrolledToBottom = true;
+
+var autoScrolling = false;
+
+Template.chat.onRendered( onRendered );
+
+function onRendered() {
+  var chat = this;
+
+  Tracker.autorun( function(){
+    // refer to data.conversation to make this rerun on update
+    if( scrolledToBottom && chat.data.conversation().length ) scrollToBottom();
+  } );
+
+  window.addEventListener( 'scroll', onScroll );
+}
+
+function scrollToBottom() {
+  console.log( 'scroll to bottom' );
+  autoScrolling = true;
+  document.body.scrollTop = document.body.scrollHeight;
+
+}
+
+var scrollTimeout;
+
+function onScroll( event ) {
+  if( autoScrolling ) {
+    autoScrolling = false;
+    return;
+  }
+  console.log( 'onScroll' );
+  var delay = 150;
+
+  if( scrollTimeout ) clearTimeout( scrollTimeout );
+  scrollTimeout = setTimeout( detectScrollPosition, delay );
+
+  function detectScrollPosition(){
+    scrollTimeout = null;
+    scrolledToBottom = document.body.scrollHeight < ( document.body.scrollTop + window.innerHeight + 30 );
+    console.log( 'setting scroll', scrolledToBottom );
+  }
+}
