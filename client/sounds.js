4df328087cfe26f564a73c20cac4da64e24ddb2e
Ivo de Kler <ivo.de.kler@gmail.com>
2016-04-23 21:04:52 +0200

    scroll/window consistency

--- client/sounds.js
+++ client/sounds.js
@@ -1,13 +1,13 @@
 sounds.load( [
-  'sounds/pop1.mp3',
-  'sounds/pop2.mp3',
-  'sounds/snare.mp3'
+  '/sounds/pop1.mp3',
+  '/sounds/pop2.mp3',
+  '/sounds/snare.mp3'
 ] );
 
 sounds.whenLoaded = console.log.bind( console, 'sounds loaded' );
 
 window.playSound = function( name ) {
-	var sound = sounds[ 'sounds/' + name + '.mp3' ];
+	var sound = sounds[ '/sounds/' + name + '.mp3' ];
 
 	if( !sound ) return console.warn( 'sound ' + name + '.mp3 not found or loaded' );
 
