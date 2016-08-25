8373dc19dbd3e7f80797b9c295a9f098b18595ed
kixxalot <joris@favie.com>
2016-08-25 12:01:07 +0200

    last minute cosmetic changes for groningen & hiding 'R' resource button in needs by edits request

--- client/user_register2.js
+++ client/user_register2.js
@@ -13,10 +13,12 @@ Template.register2.onRendered(function(){
 
     var validator = $('.register2-form').validate({
         rules: {
+            /* 2016-08-25 removed email field by edits request
             email: {
                 required: false,
                 email: true
             }
+            */
         },
         messages: {},
         submitHandler: function(event){
